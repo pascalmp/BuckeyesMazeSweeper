59c422049b011c83f3b3d39508eccfb3b8346708
laurenholloway <laurenholloway1010@gmail.com>
2016-11-14 16:54:40 -0500

    Update maze.js

--- www/js/maze.js
+++ www/js/maze.js
@@ -1,20 +1,11 @@
-var lives = 8;
+//We will need a counter for the 8 lives (buckeyes)
+
 
 //This function will turn the first block if clicked first
-function firstBlock(firstBlock){
-    document.getElementById(firstBlock).className = "done";
-}
+
 
 //This function will show the correct block and make sure they're clicked in order
-function rightBlock(previousId,currentId){
-    var lastBlock = document.getElementById(previousId);
-    var currentBlock = document.getElementById(currentId);
-    var lastClassName = lastBlock.className;
-    
-    if(lastClassName == "done"){
-        currentBlock.className = "done";
-    }
-}
+
 
 //This function will reset all the blocks if game is reset
 function resetAll(){
@@ -29,19 +20,7 @@ function resetAll(){
     
 }
 //This function is for removing a buckeye when an incorrect block is clicked
-function wrongBlock(){
-    lives = lives-1;
-    var makeId = "buck" + lives; 
-    var thisBuckeye = document.getElementById(makeId);
-    thisBuckeye.style.display ="none";
-    resetAll();
-    if(lives == 0){
-    //Make Buckeyes Section say GAME OVER
-    document.getElementById('gameOver').style.display = "inline";
-    //Make button say START instead of GO BUCKS!
-    document.getElementById("start").innerHTML = "START";
-    }
-}
+
 //This function is to make the buckeyes reappear
 function showBuckeyes(){
     lives = 8
@@ -55,14 +34,8 @@ function showBuckeyes(){
 }
 
 //This function will change the START text to GO BUCKS! when clicked
-function pressSTART() {
-    document.getElementById("start").innerHTML = "GO BUCKS!";
-    showBuckeyes();
-    document.getElementById('gameOver').style.display = "none";
-    resetAll();
-   
-    
-}
+
+
 
 
 
